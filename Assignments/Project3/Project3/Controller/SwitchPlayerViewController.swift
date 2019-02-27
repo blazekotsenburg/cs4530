dd1e1d744469c4526edee0f40a76119aa6efeafd
blazekotsenburg <u0676062@utah.edu>
2019-02-27 13:47:35 -0700

    Added new view and view controller for switching between player turns.

---
+++ Assignments/Project3/Project3/Controller/SwitchPlayerViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SwitchPlayerViewController.swift
+//  Project3
+//
+//  Created by Blaze Kotsenburg on 2/27/19.
+//  Copyright © 2019 Blaze Kotsenburg. All rights reserved.
+//
+
+import UIKit
+
+class SwitchPlayerViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Assignments/Project3/Project3/View/SwitchPlayerView.swift
@@ -0,0 +1,14 @@
+//
+//  SwitchPlayerView.swift
+//  Project3
+//
+//  Created by Blaze Kotsenburg on 2/27/19.
+//  Copyright © 2019 Blaze Kotsenburg. All rights reserved.
+//
+
+import UIKit
+
+class SwitchPlayerView: UIView {
+    
+    
+}
